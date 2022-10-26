e5e6da45e666a31a6da5de809fbfa740415a4325
tuktuks <pedro.lucas.tukinha25@gmail.com>
2022-10-26 19:08:22 -0300

    mudança de media para image

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,16 +1,40 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
-  query {
+  fragment logo on LandingPage {
+    logo {
+      data {
+        attributes {
+          url
+          alternativeText
+        }
+      }
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
+        data {
+          attributes {
+            url
+            alternativeText
+          }
+        }
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
     landingPage {
       data {
         attributes {
-          logo {
-            data {
-              attributes {
-                url
-                alternativeText
-              }
-            }
-          }
+          ...header
+          ...logo
         }
       }
     }
--- src/types/api.ts
+++ src/types/api.ts
@@ -3,8 +3,21 @@ export type LogoProps = {
     url: string;
 }
 
-export type LandingPageProps = {
-    logo{
-        logo: LogoProps
+export type HeaderProps = {
+    title: string
+    description: string
+    button {
+      label: string
+      url: string
     }
+    image {
+          url: string
+          alternativeText: string
+        } 
 }
+
+export type LandingPageProps = {
+    logo: LogoProps
+    header: HeaderProps
+}
+
